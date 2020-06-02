7488cb87a9e0515059327f0fbbb02e96d8ba261d
Red-Stalker <dnovgor@mail.ru>
2020-06-02 22:10:25 +0500

    sex

--- app/js/common.js
+++ app/js/common.js
@@ -6,3 +6,44 @@ window.onscroll = function showHeader() {
         header.classList.remove('header_fixed');
     }
 }
+
+function CheckClick(e){
+   t=e.previousSibling.previousSibling;
+   //alert(t.tagName+' '+t.type);
+   if( (t.tagName=='INPUT')&&(t.type=='checkbox')) t.click();
+}
+
+$('.buttom_reset').click(function() {
+  $('input:checked').prop('checked', false);
+  $('form :input').val('');
+});
+
+
+$("select").on("click" , function() {
+  
+  $(this).parent(".select-box").toggleClass("open");
+  
+});
+
+$(document).mouseup(function (e)
+{
+    var container = $(".select-box");
+
+    if (container.has(e.target).length === 0)
+    {
+        container.removeClass("open");
+    }
+});
+
+
+$("select").on("change" , function() {
+  
+  var selection = $(this).find("option:selected").text(),
+      labelFor = $(this).attr("id"),
+      label = $("[for='" + labelFor + "']");
+    
+  label.find(".label-desc").html(selection);
+    
+});
+
+
